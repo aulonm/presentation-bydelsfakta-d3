9f08c98c42f41d78f3ce071c4765520795f3f36b
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-09-25 21:24:42 +0200

    added revealjs and testcode

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="reveal">
+      <div class="slides">
+        <section>Single Horizontal Slide</section>
+        <section>
+          <section>Vertical Slide 1</section>
+          <section>Vertical Slide 2</section>
+        </section>
+        <page-5></page-5>
+      </div>
     </div>
-    <router-view />
   </div>
 </template>
 
+<script>
+import Reveal from 'reveal.js/js/reveal';
+import Page5 from './views/page5.vue';
+
+export default {
+  name: 'app',
+  components: { Page5 },
+  mounted() {
+    Reveal.initialize();
+  },
+};
+</script>
+
 <style lang="scss">
+@import url('../node_modules/reveal.js/css/reveal.css');
+@import url('../node_modules/reveal.js/css/theme/black.css');
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  height: 100vh;
 }
 </style>
