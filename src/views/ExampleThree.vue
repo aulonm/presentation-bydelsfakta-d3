789d3e0cc145c3540b595e0bf1a714418a26e486
Aulon Mujaj <aulon.mujaj@gmail.com>
2019-10-11 07:59:00 +0200

    almost done with example three

--- src/views/ExampleThree.vue
+++ src/views/ExampleThree.vue
@@ -44,10 +44,10 @@ export default {
       this.code =
         '' +
         "d3.select('#div')\n" +
-        "        .append('svg')\n" +
-        "        .attr('width', 420)\n" +
-        "        .attr('height', 80)\n" +
-        "        .style('background-color', 'white');";
+        "  .append('svg')\n" +
+        "  .attr('width', 420)\n" +
+        "  .attr('height', 80)\n" +
+        "  .style('background-color', 'white');";
       this.init();
     },
     steps(index) {
@@ -85,6 +85,9 @@ export default {
         case 10:
           this.step10();
           break;
+        case 11:
+          this.step11();
+          break;
         default:
           break;
       }
@@ -99,63 +102,157 @@ export default {
         .style('background-color', 'white');
       this.svg
         .append('rect')
-        .transition()
         .attr('x', 10)
         .attr('y', 10)
+        .transition()
         .attr('height', 15)
         .attr('width', 100)
         .style('background-color', 'black');
       this.svg
         .append('rect')
-        .transition()
         .attr('x', 10)
         .attr('y', 30)
+        .transition()
         .attr('height', 15)
         .attr('width', 45)
         .style('background-color', 'black');
       this.svg
         .append('rect')
-        .transition()
         .attr('x', 10)
         .attr('y', 50)
+        .transition()
         .attr('height', 15)
         .attr('width', 123)
         .style('background-color', 'black');
       this.code =
-        'let svg = d3\n' +
-        "        .select('#div3')\n" +
-        '      svg\n' +
-        "        .append('rect')\n" +
-        '        .transition()\n' +
-        "        .attr('x', 10)\n" +
-        "        .attr('y', 10)\n" +
-        "        .attr('height', 15)\n" +
-        "        .attr('width', 100)\n" +
-        "        .style('background-color', 'black');\n" +
-        '      svg\n' +
-        "        .append('rect')\n" +
-        '        .transition()\n' +
-        "        .attr('x', 10)\n" +
-        "        .attr('y', 30)\n" +
-        "        .attr('height', 15)\n" +
-        "        .attr('width', 45)\n" +
-        "        .style('background-color', 'black');\n" +
-        '      svg\n' +
-        "        .append('rect')\n" +
-        '        .transition()\n' +
-        "        .attr('x', 10)\n" +
-        "        .attr('y', 50)\n" +
-        "        .attr('height', 15)\n" +
-        "        .attr('width', 123)\n" +
-        "        .style('background-color', 'black');";
+        "let svg = d3.select('#div3')\n\n" +
+        "svg.append('rect').transition()\n" +
+        "   .attr('x', 10)\n" +
+        "   .attr('y', 10)\n" +
+        "   .attr('height', 15)\n" +
+        "   .attr('width', 100)\n" +
+        "   .style('background-color', 'black');\n" +
+        "svg.append('rect').transition()\n" +
+        "   .attr('x', 10)\n" +
+        "   .attr('y', 30)\n" +
+        "   .attr('height', 15)\n" +
+        "   .attr('width', 45)\n" +
+        "   .style('background-color', 'black');\n" +
+        "svg.append('rect').transition()\n" +
+        "   .attr('x', 10)\n" +
+        "   .attr('y', 50)\n" +
+        "   .attr('height', 15)\n" +
+        "   .attr('width', 123)\n" +
+        "   .style('background-color', 'black');";
     },
     step2() {
       this.code = 'const datapoints = [70, 150, 100, 25, 50];';
 
-      d3.select('svg').attr('height', () => this.data.length * 20 + 10);
+      d3.select('svg')
+        .transition()
+        .attr('height', () => this.data.length * 20 + 10);
 
-      let rect = d3
-        .select('svg')
+      // før var det enter - update - exit
+      // så ble det enter - exit
+      // skal du legge til ny og oppdatere gamle så måtte du bruke merge før
+      // nå bruker man bare join, så skjer enter/exit automatisk
+      // med merge så måtte du kjøre enter - merge - exit
+      // ellers så måtte du oppdatere gamle settet først og så oppdatere nye settet
+
+      // d3.select('svg')
+      //   .selectAll('rect')
+      //   .data(this.data)
+      //   .join(enter => {
+      //     return enter
+      //       .append('rect')
+      //       .attr('x', 10)
+      //       .style('background-color', 'black');
+      //   })
+      //   .attr('y', (d, i) => i * 20 + 10)
+      //   .transition()
+      //   .attr('height', 10)
+      //   .attr('width', d => d * 2);
+    },
+    step3() {
+      this.code = 'const datapoints = [70, 150, 100, 25, 50];\n\n' + '' + "d3.select('svg')\n";
+      d3.select('svg');
+    },
+    step4() {
+      this.code = 'const datapoints = [70, 150, 100, 25, 50];\n\n' + "d3.select('svg')\n" + "  .selectAll('rect')\n";
+      d3.select('svg').selectAll('rect');
+    },
+    step5() {
+      this.code =
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n';
+      d3.select('svg')
+        .selectAll('rect')
+        .data(this.data);
+    },
+    step6() {
+      this.code =
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n' +
+        '  .join(enter => {\n' +
+        '    return enter\n' +
+        "      .append('rect')\n" +
+        "      .attr('x', 10)\n" +
+        "      .style('background-color', 'black');\n" +
+        '  })\n';
+      d3.select('svg')
+        .selectAll('rect')
+        .data(this.data)
+        .join(enter => {
+          return enter
+            .append('rect')
+            .attr('x', 10)
+            .style('background-color', 'black');
+        });
+      console.log(d3.select('svg').selectAll('rect'));
+    },
+    step7() {
+      this.code =
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n' +
+        '  .join(enter => {\n' +
+        '    return enter\n' +
+        "      .append('rect')\n" +
+        "      .attr('x', 10)\n" +
+        "      .style('background-color', 'black');\n" +
+        '  })\n' +
+        "  .attr('y', (d, i) => i * 20 + 10)\n";
+      d3.select('svg')
+        .selectAll('rect')
+        .data(this.data)
+        .join(enter => {
+          return enter
+            .append('rect')
+            .attr('x', 10)
+            .style('background-color', 'black');
+        })
+        .attr('y', (d, i) => i * 20 + 10);
+    },
+    step8() {
+      this.code =
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n' +
+        '  .join(enter => {\n' +
+        '    return enter\n' +
+        "      .append('rect')\n" +
+        "      .attr('x', 10)\n" +
+        "      .style('background-color', 'black');\n" +
+        '  })\n' +
+        "  .attr('y', (d, i) => i * 20 + 10)\n" +
+        "  .attr('height', 10)\n";
+      d3.select('svg')
         .selectAll('rect')
         .data(this.data)
         .join(enter => {
@@ -165,111 +262,67 @@ export default {
             .style('background-color', 'black');
         })
         .attr('y', (d, i) => i * 20 + 10)
-        .transition()
-        .attr('height', 10)
-        .attr('width', d => d * 2);
-    },
-    step3() {
-      this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' + '      let rect = svg\n' + "        .selectAll('rect')";
-    },
-    step4() {
-      this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n';
-    },
-    step5() {
-      this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n' +
-        '        .enter()';
-      this.svg = d3.select('svg');
-      console.log(this.svg.selectAll('rect'));
-    },
-    step6() {
-      this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n' +
-        '        .enter()\n' +
-        "        .append('rect');";
-      this.svg
-        .selectAll('rect')
-        .data(this.data)
-        .enter()
-        .append('rect')
-        .attr('height', 100)
-        .style('background-color', 'black')
-        .attr('y', (d, i) => i * 20 + 10)
-        .attr('height', 10)
-        .attr('width', d => d * 2);
-    },
-    step7() {
-      this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n' +
-        '        .enter()\n' +
-        "        .append('rect');\n" +
-        '        .attr("x", 10)';
-      console.log(this.rekt);
-      this.svg
-        .selectAll('rect')
-        .data(this.data)
-        .enter()
-        .append('rect')
-        .attr('x', 0);
-    },
-    step8() {
-      this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n' +
-        '        .enter()\n' +
-        "        .append('rect');\n" +
-        '        .attr("x", 10)\n' +
-        '        .attr("y", (d, i) => i * 20 + 20)';
-      this.rect = this.rect
-        .data(this.data)
-        .enter()
-        .append('rect')
-        .attr('x', 10)
-        .style('background-color', 'black')
-        .attr('y', (d, i) => i * 20 + 10)
-        .attr('height', 10)
-        .attr('width', d => d * 2);
+        .attr('height', 10);
     },
     step9() {
       this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n' +
-        '        .enter()\n' +
-        "        .append('rect');\n" +
-        '        .attr("x", 10)\n' +
-        '        .attr("y", (d, i) => i * 20 + 20)\n' +
-        '        .attr("height", 10)';
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n' +
+        '  .join(enter => {\n' +
+        '    return enter\n' +
+        "      .append('rect')\n" +
+        "      .attr('x', 10)\n" +
+        "      .style('background-color', 'black');\n" +
+        '  })\n' +
+        "  .attr('y', (d, i) => i * 20 + 10)\n" +
+        "  .attr('height', 10)\n" +
+        "  .attr('width', d => d * 2)";
+      d3.select('svg')
+        .selectAll('rect')
+        .data(this.data)
+        .join(enter => {
+          return enter
+            .append('rect')
+            .attr('x', 10)
+            .style('background-color', 'black');
+        })
+        .attr('y', (d, i) => i * 20 + 10)
+        .attr('height', 10)
+        .attr('width', d => d * 2);
     },
     step10() {
       this.code =
-        'const datapoints = [70, 150, 100, 25, 50];\n' +
-        '      let rect = svg\n' +
-        "        .selectAll('rect')\n" +
-        '        .data(datapoints)\n' +
-        '        .enter()\n' +
-        "        .append('rect');\n" +
-        '        .attr("x", 10)\n' +
-        '        .attr("y", (d, i) => i * 20 + 20)\n' +
-        '        .attr("height", 10)\n' +
-        '        .attr("width", d => d * 2)';
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n' +
+        '  .join(enter => {\n' +
+        '    return enter\n' +
+        "      .append('rect')\n" +
+        "      .attr('x', 10)\n" +
+        "      .style('background-color', 'black');\n" +
+        '  })\n' +
+        "  .attr('y', (d, i) => i * 20 + 10)\n" +
+        "  .attr('height', 10)\n" +
+        "  .attr('width', d => d * 2)";
+    },
+    step11() {
+      this.code =
+        'const datapoints = [70, 150, 100, 25, 50];\n\n' +
+        "d3.select('svg')\n" +
+        "  .selectAll('rect')\n" +
+        '  .data(datapoints)\n' +
+        '  .join(enter => {\n' +
+        '    return enter\n' +
+        "      .append('rect')\n" +
+        "      .attr('x', 10)\n" +
+        "      .style('background-color', 'black');\n" +
+        '  })\n' +
+        "  .attr('y', (d, i) => i * 20 + 10)\n" +
+        "  .attr('height', 10)\n" +
+        "  .attr('width', d => d * 2)";
     },
     addHighlightCode(code) {
       this.removeHighlightCode();
